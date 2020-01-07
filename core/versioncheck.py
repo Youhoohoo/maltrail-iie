957a5fb2307ed6a5b20617c1f4057c44e13e5ada
Joel0x <youjz170523@163.com>
2020-01-07 15:16:35 +0800

    init

---
+++ core/versioncheck.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2019 Maltrail developers (https://github.com/stamparm/maltrail/)
+See the file 'LICENSE' for copying permission
+"""
+
+import sys
+
+PYVERSION = sys.version.split()[0]
+
+if PYVERSION < "2.6":
+    sys.exit("[%s] [CRITICAL] incompatible Python version detected ('%s'). To successfully run Maltrail you'll have to use version 2.6, 2.7 or 3.x (visit 'https://www.python.org/downloads/')" % PYVERSION)
---
+++ trails/feeds/dshieldip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2019 Maltrail developers (https://github.com/stamparm/maltrail/)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://feeds.dshield.org/top10-2.txt"
+__check__ = ".1"
+__info__ = "known attacker"
+__reference__ = "dshield.org"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line.split()[0]] = (__info__, __reference__)
+
+    return retval
